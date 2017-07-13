b2be822b853db842eb88a7639a5d9f0204061675
Todd West <twest820@gmail.com>
2017-07-12 19:05:05 -0700

    samples, tutorial: update to current bits

--- Samples/UpdateMetadata.cmd
+++
@@ -1,9 +0,0 @@
-﻿@echo off
-
-cd "Bushnell\Trophy HD Aggressor"
-"..\..\..\..\Metadata Extractor\MetadataExtractor.Tools.FileProcessor\bin\Debug\MetadataExtractor.FileProcessor.exe" --text "."
-
-cd "..\..\Reconyx\HyperFire"
-"..\..\..\..\Metadata Extractor\MetadataExtractor.Tools.FileProcessor\bin\Debug\MetadataExtractor.FileProcessor.exe" --text "."
-
-cd "..\.."
---
+++ Samples/UpdateMetadata.ps1
@@ -0,0 +1,8 @@
+﻿$fileProcessorPath = "..\..\Metadata Extractor\MetadataExtractor.Tools.FileProcessor\bin\Debug\net45\MetadataExtractor.Tools.FileProcessor.exe";
+cd "Bushnell\Trophy HD Aggressor"
+&$fileProcessorPath --text "."
+
+cd "..\..\Reconyx\HyperFire"
+&$fileProcessorPath --text "."
+
+cd "..\.."
